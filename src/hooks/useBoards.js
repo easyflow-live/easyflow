bd41f4b52c477289daf0f742dd498e5b4f4baddc
Erick Almeida <ephillipe@gmail.com>
2019-05-08 13:44:36 -0300

    teste

--- src/hooks/useBoards.js
+++ src/hooks/useBoards.js
@@ -6,30 +6,50 @@ export const useBoards = () => {
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const [boards, setBoards] = React.useState([]);
-  const { user, userRef } = useSession();  
+  const { user, userRef } = useSession();
 
   React.useEffect(() => {
-
-    const userDocRef = firebase
-    .firestore()
-    .collection(`users`)
-    .doc(user.email)
-
-    const unsubscribe = firebase
-      .firestore()
-      .collection('boards')
-      //.where("owner", "==", userDocRef)
+    const db = firebase.firestore();
+    const userDocRef = db.collection(`users`).doc(user.email)
+    const unsubscribe = db.collection('boards')
       .where("users", "array-contains", userDocRef)
-      .onSnapshot(
-        snapshot => {
-          const boards = [];
-          snapshot.forEach(doc => {
-            
-            boards.push({ ...doc.data(), uid: doc.id });
-          });
-          setBoards(boards);
-          setLoading(false);
-        },
+      .onSnapshot(snapshot => {
+        const boards = [];
+        snapshot.forEach(boardDoc => {
+          
+          db.collection('boards')
+            .doc(boardDoc.id)
+            .collection('lists')
+            .get().then(listDocs => {
+
+              const  lists = [];
+              listDocs.forEach(listDoc => {                  
+
+                db.collection('boards')
+                .doc(boardDoc.id)
+                .collection('lists')
+                .doc(listDoc.id)
+                .collection('cards')
+                .get().then(cardDocs => {
+                  debugger;
+                  const cards = [];
+                  cardDocs.forEach(cardDoc => {
+                    cards.push({ ...cardDoc.data(), uid: cardDoc.id });               
+                  });
+                  lists.push({ ...listDoc.data(), uid: listDoc.id, cards }); 
+                  
+                  boards.push({ ...boardDoc.data(), uid: boardDoc.id, lists });
+                  setBoards(boards);
+                  setLoading(false);
+                  
+                }); 
+                              
+              });
+              
+            });  
+
+        });        
+      },
         err => {
           setError(err);
           setLoading(false);
