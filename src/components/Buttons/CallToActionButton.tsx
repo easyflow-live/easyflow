be9cfc149e7bd4b2a11f1c90511452e8ad453459
delete <pinheiro.llip@gmail.com>
2019-05-20 11:07:07 -0300

    Improve Add tag and member buttons

--- src/components/Buttons/CallToActionButton.tsx
+++ src/components/Buttons/CallToActionButton.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface CallToActionButtonProps {
   children: React.ReactChild;
+  className: string;
 }
 
-export default ({ children, ...props }: CallToActionButtonProps) => (
+export default ({ children, className, ...props }: CallToActionButtonProps) => (
   <button
-    className='rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-pink-500 hover:bg-pink-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md cursor-pointer'
+    className={`rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-pink-500 hover:bg-pink-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md cursor-pointer ${className}`}
     {...props}
   >
     {children}
