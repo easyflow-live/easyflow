197e75a843d2b9397852695e3bb749e038614dbb
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2019-05-22 20:42:53 -0300

    Improve board buttons accessibility making the button a native button element

--- src/components/BoardHeader/BoardAddMember.jsx
+++ src/components/BoardHeader/BoardAddMember.jsx
@@ -5,25 +5,29 @@ import AddMemberModal from './AddMemberModal';
 
 const BoardAddMember = ({ boardId }) => {
   const buttonRef = useRef(null);
-  const [ isOpen, setIsOpen ] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="board-wrapper">
-      <span className="board-button" ref={buttonRef} onClick={() => setIsOpen(true)}>
-        <div className="modal-icon">
+    <div className='board-wrapper'>
+      <button
+        className='board-button'
+        ref={buttonRef}
+        onClick={() => setIsOpen(true)}
+      >
+        <div className='modal-icon'>
           <FaUsers />
         </div>
-        <div className="board-header-right-text">&nbsp;Add member</div>
-      </span>
+        <div className='board-header-right-text'>&nbsp;Add member</div>
+      </button>
 
-      <AddMemberModal 
+      <AddMemberModal
         isOpen={isOpen}
         buttonElement={buttonRef.current}
         boardId={boardId}
         toggleCardEditor={() => setIsOpen(!isOpen)}
-        />
+      />
     </div>
   );
-}
+};
 
 export default BoardAddMember;
