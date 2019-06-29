49bf1dcefe67b51da068b706a510379fef5f8bbe
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2019-06-29 00:12:53 -0300

    Use Link component from Next to avoid SSR

--- src/components/Board/BoardLink.tsx
+++ src/components/Board/BoardLink.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import './BoardLink.css';
 
-export default ({ board, ...props }) => {
+export default ({ board, href, ...props }) => {
   return (
-    <a
-      className='board-link bg-gray-700 hover:bg-gray-600 shadow-lg rounded-lg p-4 m-2 w-full cursor-pointer break-words'
-      {...props}
-    >
-      <span className='text-white'>{board.title}</span>
-    </a>
+    <Link href={href}>
+      <a
+        className='board-link bg-gray-700 hover:bg-gray-600 shadow-lg rounded-lg p-4 m-2 w-full cursor-pointer break-words'
+        {...props}
+      >
+        <span className='text-white'>{board.title}</span>
+      </a>
+    </Link>
   );
 };
