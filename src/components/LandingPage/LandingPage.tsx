037ca09736ba3ae288cb6a3cf32c0270119be1f9
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2019-08-21 21:46:30 -0300

    Hide login button and shows a warning on mobile Safari.
    Firebase redirect has a bug with iOS Safari.

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Title } from 'react-head';
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 import { useGoogleLogin } from '../../hooks/useLogin';
+import { useIsMobileSafari } from '../../hooks/useIsMobileSafari';
 import CallToActionButton from '../Buttons/CallToActionButton';
-import Pricing from './Pricing';
 import Header from './Header';
 import Footer from './Footer';
 import { useTrail, animated } from 'react-spring';
@@ -15,6 +16,20 @@ import './LandingPage.scss';
 
 const items = ['Simple.', 'Beautiful.', 'Darker.'];
 const config = { mass: 5, tension: 2000, friction: 800 };
+
+const SafariWarning = ({ isMobileSafari, onLogin }) =>
+  isMobileSafari ? (
+    <p className='text-red-500 '>
+      <FaExclamationTriangle className='inline mr-1' />
+      <span>
+        This app is unavailable when using Safari on IPhone because of iOS
+        native issues. Please use the app from Google Chrome or Firefox. We hope
+        to improve this as soon as possible.
+      </span>
+    </p>
+  ) : (
+    <CallToActionButton onClick={onLogin}>Get Started</CallToActionButton>
+  );
 
 const LandingPage = () => {
   const { login } = useGoogleLogin();
@@ -29,6 +44,9 @@ const LandingPage = () => {
       transform: 'translate3d(0,0px,0)',
     },
   });
+  const isMobileSafari = useIsMobileSafari();
+
+  const handleLogin = () => login();
 
   return (
     <main>
@@ -73,9 +91,10 @@ const LandingPage = () => {
                     complete tasks and ship great products.
                   </p>
                   <div className='flex mt-6 justify-start md:justify-center xl:justify-start'>
-                    <CallToActionButton onClick={login}>
-                      Get Started
-                    </CallToActionButton>
+                    <SafariWarning
+                      isMobileSafari={isMobileSafari}
+                      onLogin={handleLogin}
+                    />
                   </div>
                 </>
               </AnimatedSlideUp>
@@ -93,7 +112,7 @@ const LandingPage = () => {
       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
         <path
           fill='rgba(74, 85, 104, 0.458824)'
-          fill-opacity='1'
+          fillOpacity='1'
           d='M0,32L80,69.3C160,107,320,181,480,192C640,203,800,149,960,122.7C1120,96,1280,96,1360,96L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'
           data-darkreader-inline-fill=''
         />
@@ -151,7 +170,7 @@ const LandingPage = () => {
       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
         <path
           fill='#4a556875'
-          fill-opacity='1'
+          fillOpacity='1'
           d='M0,96L80,112C160,128,320,160,480,154.7C640,149,800,107,960,74.7C1120,43,1280,21,1360,10.7L1440,0L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z'
           data-darkreader-inline-fill=''
         />
@@ -165,7 +184,10 @@ const LandingPage = () => {
                 Start today and get more done in a simple way
               </h2>
             </AnimateSlideUpinView>
-            <CallToActionButton onClick={login}>Get Started</CallToActionButton>
+            <SafariWarning
+              isMobileSafari={isMobileSafari}
+              onLogin={handleLogin}
+            />
           </div>
         </div>
       </section>
