0084f7747a139760a932d68d78ee8c767a563e12
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2019-09-18 22:51:30 -0300

    update all packages

--- pages/_app.js
+++ pages/_app.js
@@ -17,15 +17,6 @@ import '../src/styles/style.css';
 
 export default observer(
   class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-      let pageProps = {};
-
-      if (Component.getInitialProps) {
-        pageProps = await Component.getInitialProps(ctx);
-      }
-      return { pageProps };
-    }
-
     constructor() {
       super();
       this.unsubscribe = () => {};
