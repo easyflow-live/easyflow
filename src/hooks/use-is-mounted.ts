3824b1e8c95ea1deb83471161e5dbf4b5797d253
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2019-09-24 19:58:34 -0300

    improve hook

--- src/hooks/use-is-mounted.ts
+++ src/hooks/use-is-mounted.ts
@@ -1,9 +1,13 @@
 import { useRef, useEffect } from 'react';
 
 export const useIsMounted = (): boolean => {
-  const isMounted = useRef(true);
-  useEffect(() => () => {
-    isMounted.current = false;
-  });
+  const isMounted = useRef(false);
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
   return isMounted.current;
 };
