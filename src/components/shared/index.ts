31ced05f80c53514bc0317f8a4538fc7a446900f
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-02-19 20:39:47 -0300

    fix: Remove import to remove compile error

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,7 +2,6 @@ export { default as Avatar } from './Avatar';
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
-export { default as ClickOutSide } from './ClickOutSide';
 export { default as Divider } from './Divider';
 export { default as Editable } from './Editable';
 export { default as Heading } from './Heading';
