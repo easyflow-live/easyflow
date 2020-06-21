e7030d1051905d82e35474772361eb03a3584c5c
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-06-21 18:46:05 -0300

    feat(input): add disabled and loading prop state with loading icon

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,5 +1,6 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { DetailedHTMLProps, InputHTMLAttributes, ReactChild } from 'react';
 import classNames from 'classnames';
+import { Spinner } from './Spinner';
 
 export interface InputProps
   extends DetailedHTMLProps<
@@ -7,17 +8,32 @@ export interface InputProps
     HTMLInputElement
   > {
   full?: boolean;
+  loading?: boolean;
 }
 
-const Input = ({ className, full = true, ...props }: InputProps) => (
-  <input
-    className={classNames(
-      'bg-gray-600 shadow appearance-none rounded py-2 px-3 text-white leading-tight placeholder-gray-900',
-      full ? 'w-full' : '',
-      className
-    )}
-    {...props}
-  />
+const Input = ({
+  className,
+  full = true,
+  disabled,
+  loading,
+  ...props
+}: InputProps) => (
+  <div className='inline relative'>
+    <input
+      disabled={disabled}
+      className={classNames(
+        'bg-gray-600 shadow appearance-none rounded py-2 px-3 text-white leading-tight placeholder-gray-900',
+        {
+          'bg-gray-700 text-gray-900 cursor-not-allowed': disabled,
+          'w-full': full,
+        },
+        className
+      )}
+      {...props}
+    />
+
+    {loading && <Spinner className='absolute top-0 right-0 mr-2' />}
+  </div>
 );
 
 export default Input;
