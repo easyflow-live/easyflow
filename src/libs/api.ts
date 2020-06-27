144a65e3f5d80efb1bf26a9ba27640dd8959fecc
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-06-27 13:00:19 -0300

    feat(invite-email): add an API url to send invites to a team board

---
+++ src/libs/api.ts
@@ -0,0 +1,18 @@
+type InviteEmail = {
+  to: string;
+  userName: string;
+  userEmail: string;
+  ownerName: string;
+  boardName: string;
+  boardUrl: string;
+};
+
+export const sendInviteEmail = (body: InviteEmail) => {
+  return fetch('/api/invite', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+};
