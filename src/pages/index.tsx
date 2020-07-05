136b48ec18142664c21e6a602845ec8ea97fc27a
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-07-05 14:21:53 -0300

    feat: show loader if it is redirecting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Home from '../components/Home/Home';
 import AuthenticatedPage from '../components/shared/AuthenticatedPage';
 import { useSession } from '../hooks/use-session';
+import { useRouter } from 'next/router';
 
 export default () => {
   const { userDoc } = useSession();
+  const { query } = useRouter();
+  const { redirect } = query;
 
   return (
-    <AuthenticatedPage>
+    <AuthenticatedPage redirect={redirect === 'true' ? true : false}>
       <Home userDoc={userDoc} />
     </AuthenticatedPage>
   );
