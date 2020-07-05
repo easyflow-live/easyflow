26037ee0efed66d059cb4d6fd909a2ed22bc0642
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-07-05 14:25:51 -0300

    fix: cb types

--- src/hooks/use-google-login.ts
+++ src/hooks/use-google-login.ts
@@ -2,10 +2,12 @@ import { useCallback } from 'react';
 
 import firebase from '../services/firebase.service';
 
-const noop = () => null;
-
 export const useGoogleLogin = () => {
-  const login = useCallback((cb = noop) => firebase.doSignInWithGoogle(cb), []);
+  const login = useCallback(
+    (cb: (user?: firebase.User) => Promise<void>) =>
+      firebase.doSignInWithGoogle(cb),
+    []
+  );
   const logout = useCallback(() => firebase.doSignOut(), []);
 
   return { login, logout };
