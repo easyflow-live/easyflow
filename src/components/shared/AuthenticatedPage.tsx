d2b6bb971a69aef66e49f0de5c3a823e58634996
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-07-05 14:21:53 -0300

    feat: show loader if it is redirecting

--- src/components/shared/AuthenticatedPage.tsx
+++ src/components/shared/AuthenticatedPage.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react';
-import Router from 'next/router';
 import { ErrorProps } from 'next/error';
 import Error from 'next/error';
 
@@ -17,11 +16,9 @@ interface PageProps {
 export default ({ children, isAnonymous, redirect, error }: PageProps) => {
   const { user, initializing } = useSession();
 
-  if (initializing) return <Loader />;
+  if (initializing || redirect) return <Loader />;
   if (error) return <Error {...error} />;
   if (isAnonymous || user) return children;
-  if (!redirect) return <LandingPage />;
 
-  Router.push('/');
-  return null;
+  return <LandingPage />;
 };
