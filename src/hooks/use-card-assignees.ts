fd04ded67992a6baddcb9ce53f93cd14b7624188
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-08-18 18:45:55 -0300

    refactor: avoid unnecessary re-renders

--- src/hooks/use-card-assignees.ts
+++ src/hooks/use-card-assignees.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import CardDocument from '../../src/documents/card.doc';
 import { useUsersStore } from '../store';
@@ -6,20 +6,29 @@ import { sortUsersAlpha } from '../helpers/sortUsersAlpha';
 
 export const useCardAssignees = (card: CardDocument) => {
   const { users, loadUsers } = useUsersStore();
-
-  // (backwards compatibility)
-  if (card.data.assignee && !Array.isArray(card.data.assignee)) {
-    card.data.assignee = [card.data.assignee];
-  }
-
-  const ids = (card.data.assignee && card.data.assignee.map(a => a.id)) || [];
-  const assignees = users.filter(u => ids.includes(u.id)).sort(sortUsersAlpha);
+  const [assignees, setAssignees] = useState([]);
 
   useEffect(() => {
+    // (backwards compatibility)
+    if (card.data.assignee && !Array.isArray(card.data.assignee)) {
+      card.data.assignee = [card.data.assignee];
+    }
+
     if (card.data.assignee) {
       loadUsers(card.data.assignee);
     }
   }, [card.data.assignee, loadUsers]);
+
+  useEffect(() => {
+    if (card.data.assignee) {
+      const ids = card.data.assignee.map(a => a.id);
+      const assignees = users
+        .filter(u => ids.includes(u.id))
+        .sort(sortUsersAlpha);
+
+      setAssignees(assignees);
+    }
+  }, [users]);
 
   return { assignees };
 };
