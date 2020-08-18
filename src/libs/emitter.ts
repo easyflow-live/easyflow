87890ab2862606ce8761298621e488e38f72ee4d
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-08-18 20:39:14 -0300

    feat(emitter): add types to card actions

--- src/libs/emitter.ts
+++ src/libs/emitter.ts
@@ -9,6 +9,42 @@ export type EventSubscription = Susbcription;
 export interface EmitterTypes {
   ASSIGNEE_UPDATED: { cardId: string };
   TEAM_MEMBER_UPDATED: {};
+  REMOVE_CARD: {
+    title: string;
+    text: string;
+    listId: string;
+  };
+  COMPLETE_CARD: {
+    title: string;
+    listId: string;
+    completed: boolean;
+    cardId: string;
+  };
+  ASSIGNE_CARD: {
+    title: string;
+    listId: string;
+    assigneId: string;
+    cardId: string;
+  };
+  ADD_CARD: {
+    title: string;
+    listId: string;
+    cardId: string;
+  };
+  EDIT_CARD: {
+    oldText: string;
+    newText: string;
+    oldTitle: string;
+    newTitle: string;
+    listId: string;
+    cardId: string;
+  };
+  MOVE_CARD: {
+    listBeforeId: string;
+    listAfterId: string;
+    cardId: string;
+    title: string;
+  };
 }
 
 const _emitter = new Emitter();
@@ -21,6 +57,7 @@ export const emitter = {
     return _emitter.addListener(key, listener);
   },
   emit<K extends keyof EmitterTypes>(key: K, payload: EmitterTypes[K]) {
+    if (__DEV__) console.debug('[EMITTER]', key, payload); // tslint:disable-line no-console
     _emitter.emit(key, payload);
   },
 };
