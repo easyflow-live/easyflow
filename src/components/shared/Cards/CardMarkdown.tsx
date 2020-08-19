9d2d52470f4215b0a973ece8d36625034104f615
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-08-18 22:26:46 -0300

    refactor(card): move markdown logic to CardMarkdown

--- src/components/shared/Cards/CardMarkdown.tsx
+++ src/components/shared/Cards/CardMarkdown.tsx
@@ -1,17 +1,49 @@
 import React, { FC } from 'react';
+import { observer } from 'mobx-react-lite';
 
 import MarkdownText from 'components/shared/MarkdownText';
+import { useMarkdownCheckbox } from 'hooks/use-markdown-checkbox';
 import CardBasic, { CardBasicProps } from './CardBasic';
-import { observer } from 'mobx-react-lite';
 
 interface CardMarkdownProps extends CardBasicProps {
   text: string;
+  onChangeCheckbox: (text: string) => void;
 }
 
-const CardMarkdown: FC<CardMarkdownProps> = props => (
-  <CardBasic {...props}>
-    <MarkdownText source={props.text} />
-  </CardBasic>
-);
+const isLink = (tagName: string) => tagName.toLowerCase() === 'a';
+const isInput = (tagName: string) => tagName.toLowerCase() === 'input';
+
+const CardMarkdown: FC<CardMarkdownProps> = ({
+  text,
+  onChangeCheckbox,
+  ...props
+}) => {
+  const toggleCheckbox = useMarkdownCheckbox(text);
+
+  const changeCheckbox = (checked: boolean, index: number) => {
+    const newText = toggleCheckbox({
+      checked,
+      index,
+    });
+
+    onChangeCheckbox(newText);
+  };
+
+  const handleClick = e => {
+    const { tagName, checked, id } = e.target;
+
+    if (isInput(tagName)) {
+      changeCheckbox(checked, parseInt(id));
+    } else if (!isLink(tagName)) {
+      props.onClick(e);
+    }
+  };
+
+  return (
+    <CardBasic {...props} onClick={handleClick}>
+      <MarkdownText source={text} />
+    </CardBasic>
+  );
+};
 
 export default observer(CardMarkdown);
