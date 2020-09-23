630dbffb27c7e38761b28039a4e07b19bd11f40d
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-09-22 21:03:23 -0300

    chore: fix root import

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '../src'),
+      ...config.resolve.modules,
+    ];
+
+    return config;
+  },
 };
