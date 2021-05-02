e2503374e9fed5fd43509cff8dffe82b53b8e69e
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2021-05-02 19:18:03 -0300

    chore: add user following user table

---
+++ packages/backend/migrations/default/1619993681147_add_user_following_user_table/up.sql
@@ -0,0 +1,2 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE TABLE "public"."user_following_user"("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "user_id" uuid NOT NULL, "following_id" uuid NOT NULL, "follow" boolean NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON UPDATE restrict ON DELETE cascade, FOREIGN KEY ("following_id") REFERENCES "public"."users"("id") ON UPDATE restrict ON DELETE cascade);
