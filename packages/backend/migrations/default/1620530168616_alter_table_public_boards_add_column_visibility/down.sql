77711cd96b327d3ada23127d09800263a4ae406d
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2021-05-09 00:22:12 -0300

    chore: add visibility column to boards

---
+++ packages/backend/migrations/default/1620530168616_alter_table_public_boards_add_column_visibility/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."boards" add column "visibility" varchar
+ not null default 'public';
---
+++ packages/backend/migrations/default/1620530168616_alter_table_public_boards_add_column_visibility/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."boards" add column "visibility" varchar
+ not null default 'public';
