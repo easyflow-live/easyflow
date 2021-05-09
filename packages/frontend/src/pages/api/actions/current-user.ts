040041f81f4b914d0364c83a9c9b65b575ce23e8
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2021-05-08 21:17:18 -0300

    chore: return 404 is user does not exist

--- packages/frontend/src/pages/api/actions/current-user.ts
+++ packages/frontend/src/pages/api/actions/current-user.ts
@@ -1,14 +1,18 @@
-import { NextApiRequest } from 'next'
-import { getAuthenticatedUser } from 'server/auth/getAuthenticatedUser'
-import { verifyActionToken } from 'server/auth/verifyActionToken'
-import prisma from 'server/db/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getAuthenticatedUser } from 'src/server/auth/getAuthenticatedUser'
+import { verifyActionToken } from 'src/server/auth/verifyActionToken'
+import prisma from 'src/server/db/prisma'
 
-export default async (req: NextApiRequest, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   verifyActionToken(req)
   const authUser = getAuthenticatedUser(req)
   const user = await prisma.users.findUnique({
     where: { id: authUser.id },
   })
+
+  if (!user) {
+    return res.status(404)
+  }
 
   return res.send({
     name: user.name,
